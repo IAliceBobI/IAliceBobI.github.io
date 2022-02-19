5ec55a68995dccc74c70d092154dc7eb13fb2ee7
alicebob <alicebob@gmail.com>
2022-02-19 10:43:23 +0800

    s

--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -8,5 +8,5 @@ sudo apt-get install nginx git build-essential fzf fonts-powerline zsh autojump 
 export HOSTNAME=$(curl -s http://169.254.169.254/metadata/v1/hostname)
 export PUBLIC_IPV4=$(curl -s http://169.254.169.254/metadata/v1/interfaces/public/0/ipv4/address)
 
-# sh -c "$(curl -fsSL https://www.cryptonftday.com/provisioning.sh)"
+# sh -c "$(curl -fsSL https://www.cryptonftday.com/scripts/provisioning.sh)"
 
